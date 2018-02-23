8534bac076b890d4585c4d8868f096205897f3c3
Kaelyn Arnold <krjarnold@gmail.com>
2018-02-23 15:57:08 -0500

    First attemps

---
+++ animated_avatar.js
@@ -0,0 +1,5 @@
+mySVG = document.querySelector('.svgContainer')
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("ninja").setAttribute("color", "red");
+  document.querySelector('.eye1').setAttribute("color", "red");
+});
